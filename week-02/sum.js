597184608cb1558859b34ddcab825df43f27b123
wanchichang <a0965266902@gmail.com>
2024-09-20 14:57:30 +0800

    feat: add ary.reduce

--- week-02/sum.js
+++ week-02/sum.js
@@ -1,12 +1,29 @@
 // ary: number array
-function sum(ary) {
-  // TODO: sum all elements in ary
+// TODO: sum all elements in ary
+
+function sum_forEach(ary) {
+  //method 1: ary.forEach
   let ans = 0
   ary.forEach(function (item) {
     ans += item
-    // ... do something with an item
   })
   return ans
 }
+function sum_reduce(ary) {
+  //method 2: ary.reduce
+  //example:
+  //let value = arr.reduce(function(accumulator, item, index, array) {
+  // ...
+  // }, [initial]);
 
-console.log(sum([1, 5, 3, 2])) // 11
+  let ans = ary.reduce((sum, current) => sum + current, 0)
+
+  return ans
+}
+
+console.log('method 1: ary.ForEach, ans:')
+console.log(sum_forEach([1, 5, 3, 2])) // 11
+console.log(sum_forEach([1, 4, 2, 7, 10])) // 24
+console.log('method 2: ary.reduce, ans:')
+console.log(sum_reduce([1, 5, 3, 2])) // 11
+console.log(sum_reduce([1, 4, 2, 7, 10])) // 24
